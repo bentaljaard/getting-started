cb07afb3f12dbd6c5444b7c71b1d29246a4380e6
Ben Taljaard <ben.taljaard@devoteam.com>
2020-04-15 20:34:16 +0200

    Initial commit

---
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -0,0 +1,30 @@
+package org.acme.getting.started;
+
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api")
+public class GreetingResource {
+
+    @Inject
+    GreetingService greetingService;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(@PathParam String name) {
+        return greetingService.greeting(name);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
